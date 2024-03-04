e0ff71e9285764687364af0f721e792bbf26170f
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2024-03-05 03:02:15 +0500

    ./

--- src/components/Sneakers.jsx
+++ src/components/Sneakers.jsx
@@ -30,10 +30,10 @@ function Sneakers() {
   return (
     <div>
       <div className="carousel class w-full">
-        <div id="slide1" className="carousel-item relative w-full">
+        <div id="slide1" className="carousel-item relative  w-full">
           <img src="/big-sneakers.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle left carousel-control">
+          <div className="flex absolute justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn  btn-circle left  carousel-control">
               ❮
             </a>
             <a href="#slide2" className="btn btn-circle">
@@ -43,7 +43,7 @@ function Sneakers() {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img src="/first-sneakers.jpg" className="w-full rounded-none" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className=" flex absolute justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
             </a>
@@ -52,9 +52,9 @@ function Sneakers() {
             </a>
           </div>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
+        <div id="slide3" className="carousel-item relative  w-full">
           <img src="/second-sneakers.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className=" flex absolute justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
             </a>
@@ -63,9 +63,9 @@ function Sneakers() {
             </a>
           </div>
         </div>
-        <div id="slide4" className="carousel-item relative w-full">
+        <div id="slide4" className="carousel-item relative  w-full">
           <img src="/third-sneakers.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className=" flex absolute justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
             </a>
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,7 +5,70 @@ function Navbar() {
     <div className="container">
       <div className="nav-wrapper-1 pt-[28px] flex justify-between">
         <div className="items-wrapper flex gap-x-[56.5px]">
-        <img className="hidden cursor-pointer mt-[4px] h-[15px]" src="/burger.svg" alt="" />
+          <div className="drawer  w-[16px]">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <label htmlFor="my-drawer">
+                <img
+                  className="hidden cursor-pointer mt-[4px] h-[15px]"
+                  src="/burger.svg"
+                  alt=""
+                />
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay absolute"
+              ></label>
+              <ul className="menu p-[24px] w-[250px] min-h-full bg-white flex gap-[20px] text-[15px] leading-[26px] text-gray">
+                <label
+                  htmlFor="my-drawer"
+                  aria-label="close sidebar"
+                  to={"/"}
+                  className="drawer-overlay"
+                >
+                  <img
+                    className="mb-[33.73px]"
+                    src="/x-image.svg"
+                    width={13.44}
+                    alt=""
+                  />
+                </label>
+                <NavLink
+                  to={"/"}
+                  className="font-bold text-[18px] leading-[26px] text-block"
+                >
+                  Collection
+                </NavLink>
+                <NavLink
+                  to={"/men"}
+                  className="font-bold text-[18px] leading-[26px] text-block"
+                >
+                  Men
+                </NavLink>
+                <NavLink
+                  to={"/women"}
+                  className="font-bold text-[18px] leading-[26px] text-block"
+                >
+                  Women
+                </NavLink>
+                <NavLink
+                  to={"/about"}
+                  className="font-bold text-[18px] leading-[26px] text-block"
+                >
+                  About
+                </NavLink>
+                <NavLink
+                  to={"/contact"}
+                  className="font-bold text-[18px] leading-[26px] text-block"
+                >
+                  Contact
+                </NavLink>
+              </ul>
+            </div>
+          </div>
           <NavLink to={"/"}>
             <img src="/sneakers-logo.svg" alt="" />
           </NavLink>
@@ -60,7 +123,11 @@ function Navbar() {
             src="/Shop.svg"
             alt=""
           />
-          <img className="cursor-pointer h-[50px] avatar" src="/Avatar.svg" alt="" />
+          <img
+            className="cursor-pointer h-[50px] avatar"
+            src="/Avatar.svg"
+            alt=""
+          />
         </div>
       </div>
       <hr className="text-[#E4E9F2] nav-wrapper mb-[90px]" />
